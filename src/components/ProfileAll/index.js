7163fd823e2aed73288204adb0d2516a39b3464f
이우빈 <125638564+dnqls9875@users.noreply.github.com>
2024-12-11 22:26:33 +0900

    [feat] profileAll ui 구현
    - profileAll 컴포넌트 ui 구현
    - Pretendard font 파일 삭제 및 변경
    - gitignore /docs 추가
    - images 폴더 public 으로 이동
    - reset.js 파일 생성 후 reset.css 내용 추가 보완

---
+++ src/components/ProfileAll/index.js
@@ -0,0 +1,1 @@
+import '@/components/ProfileAll/profileAll';
---
+++ src/components/ProfileAll/profileAll.js
@@ -0,0 +1,71 @@
+import '@/assets/font/pretendard/pretendard.css';
+import { LitElement, html } from 'lit';
+import { profileAllStyles } from './profileAllCss';
+import resetStyles from '@/styles/reset.js';
+
+class ProfileAll extends LitElement {
+  constructor() {
+    super();
+  }
+
+  static styles = [resetStyles, profileAllStyles];
+
+  render() {
+    return html`
+      <section class="profile">
+        <h2 class="a11y-hidden">프로필</h2>
+        <div class="profile-container">
+          <div class="profile-container__top">
+            <div class="avatar">
+              <button type="button">img</button>
+              <img src="/images/ico_write_sm.png" alt="프로필 수정 이미지" />
+            </div>
+            <div class="user-info">
+              <p class="nickname">유저닉네임</p>
+              <div class="review">
+                <ul>
+                  <li>
+                    <a href="/">
+                      <span>리뷰</span>
+                      <span>17</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/">
+                      <span>사진</span>
+                      <span>19</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/">
+                      <span>팔로잉</span>
+                      <span>0</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/">
+                      <span>팔로워</span>
+                      <span>2</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="profile-container__bottom">
+            <button type="button">피드작성</button>
+            <button type="button">리뷰작성</button>
+          </div>
+        </div>
+        <div class="coupon">
+          <a href="/" class="coupon-link">
+            <span class="coupon-text">쿠폰</span>
+            <span class="coupon-count">1</span>
+          </a>
+        </div>
+      </section>
+    `;
+  }
+}
+
+customElements.define('profileall-element', ProfileAll);
