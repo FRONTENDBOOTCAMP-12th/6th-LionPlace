066a9c0a968317d43c8183612a7705174612da02
Bzzzang <sindmsql@gmail.com>
2024-12-18 17:10:11 +0900

    [modify] 오타 수정
    - 오타 수정

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -16,7 +16,7 @@ export class FeedPage extends LitElement {
       {
         profileImage: '/images/img_profile.jpg',
         username: '멋쟁이사자처럼',
-        date: '사자리뷰 2 ・ 3.1.수',
+        date: '사진리뷰 2 ・ 3.1.수',
         postImage: '/images/img_product1.jpg',
         content:
           '주말엔 매일 줄 서 있어서 먹을 수가 없었는데 평일에 다행히 갈 수 있어서 너무 좋았어요! 좀 안기다리고 먹었는데 와 너무 맛있었습니다!! 괜히 또간집에서 나온 곳이 아니구나...',
@@ -24,7 +24,7 @@ export class FeedPage extends LitElement {
       {
         profileImage: '/images/img_profile.jpg',
         username: '멋쟁이사자처럼',
-        date: '사자리뷰 2 ・ 3.1.수',
+        date: '사진리뷰 2 ・ 3.1.수',
         postImage: '/images/img_product1.jpg',
         content:
           '주말엔 매일 줄 서 있어서 먹을 수가 없었는데 평일에 다행히 갈 수 있어서 너무 좋았어요! 좀 안기다리고 먹었는데 와 너무 맛있었습니다!! 괜히 또간집에서 나온 곳이 아니구나...',
