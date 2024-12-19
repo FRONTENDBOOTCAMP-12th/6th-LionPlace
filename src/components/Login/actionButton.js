09e87ad24ea45f6ffcf5cc2ec4a74b51544ba599
dlgudwn94 <dlgudwn82@naver.com>
2024-12-19 15:42:54 +0900

    [feat] (임시) 로그인 기능 구현

---
+++ src/components/Login/actionButton.js
@@ -0,0 +1,34 @@
+import { LitElement, html, css } from 'lit';
+import resetStyles from '@/styles/reset.js';
+
+class ActionButton extends LitElement {
+  static properties = {
+    text: { type: String },
+    onClick: { type: Function },
+  };
+
+  static styles = [
+    resetStyles,
+    css`
+      button {
+        width: 100%;
+        height: 5vh;
+        margin-top: 1rem;
+        padding: 1rem;
+        background-color: white;
+        border: none;
+        border-radius: 6px;
+        color: #1a1f2e;
+        font-size: 1rem;
+        font-weight: bold;
+        cursor: pointer;
+      }
+    `,
+  ];
+
+  render() {
+    return html` <button @click="${this.onClick}">${this.text}</button> `;
+  }
+}
+
+customElements.define('action-button', ActionButton);
