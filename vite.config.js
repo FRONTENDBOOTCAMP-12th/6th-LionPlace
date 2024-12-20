e347181919d735de095d39aeece1852f9e930a78
jiyoung <aa991102@naver.com>
2024-12-20 11:49:08 +0900

    [chore] vite.config.js 페이지 엔트리 포인트 추가

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'src/pages/login/index.html'),
+        signUp: resolve(__dirname, 'src/pages/sign-up/index.html'),
+        reserved: resolve(__dirname, 'src/pages/reserved/index.html'),
+        review: resolve(__dirname, 'src/pages/review/index.html'),
+        feed: resolve(__dirname, 'src/pages/feed/index.html'),
+        savedPlaces: resolve(__dirname, 'src/pages/saved-places/index.html'),
       },
     },
   },
