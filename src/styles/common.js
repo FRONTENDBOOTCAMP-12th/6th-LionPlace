6ee9b1e4878014bb8195fe0bd302c72439940c79
jiyoung <aa991102@naver.com>
2024-12-26 18:04:37 +0900

    [Rename] reset.js -> common.js

--- src/styles/common.js
+++ src/styles/common.js
@@ -161,6 +161,7 @@ export default css`
     display: none;
   }
 
+  /* Accessibility */
   caption,
   legend,
   .a11y-hidden {
@@ -170,5 +171,35 @@ export default css`
     width: 1px;
     height: 1px;
     clip: rect(1px, 1px, 1px, 1px);
+  }
+
+  /* Button */
+  .btn {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    font-family: 'Pretendard Variable', Pretendard, sans-serif;
+
+    &.base {
+      inline-size: 17.875rem;
+      padding: 0.75rem 6.25rem;
+    }
+    &.black {
+      background-color: var(--contentPrimary);
+      color: var(--white);
+    }
+    &.ico1::before {
+      content: '';
+      inline-size: 1.1875rem;
+      block-size: 1.125rem;
+      background: url(/images/ico_write.png);
+    }
+    &.rounded {
+      border-radius: 0.5rem;
+    }
   }
 `;
