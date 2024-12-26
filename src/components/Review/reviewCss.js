ba7eb2acf09b61cb412d1b8c494490d47a6d2469
jiyoung <aa991102@naver.com>
2024-12-26 18:14:48 +0900

    [Modify] 참조 파일(buttonCss.js) 삭제

--- src/components/Review/reviewCss.js
+++ src/components/Review/reviewCss.js
@@ -1,10 +1,8 @@
 import { css } from 'lit';
 import commonStyles from '@/styles/common.js';
-import { buttonStyles } from '../Button/buttonCss.js';
 
 export const reviewStyles = [
   commonStyles,
-  buttonStyles,
   css`
     .btn {
       padding: 0.625rem 1.125rem !important;
