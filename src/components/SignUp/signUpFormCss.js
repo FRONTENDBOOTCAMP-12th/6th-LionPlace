60eb3e8b78cf99eb476d92a97bbb139ccf73aa4f
dlgudwn94 <dlgudwn82@naver.com>
2024-12-31 12:40:49 +0900

    [modify] 회원가입 페이지 css 수정

--- src/components/SignUp/signUpFormCss.js
+++ src/components/SignUp/signUpFormCss.js
@@ -1,16 +1,21 @@
 import { css } from 'lit';
 
 export const signUpFormCss = css`
-  .container {
+  .sign-up-section {
     max-width: 30rem;
     margin: 0 auto;
     padding: 2rem;
+    height: 100vh;
   }
 
-  h1 {
+  .sign-up-section h1 {
     font-size: 2rem;
     font-weight: 600;
     margin-bottom: 2rem;
     color: var(--white);
+  }
+
+  .sign-up-section submit-button {
+    margin-bottom: 2rem;
   }
 `;
