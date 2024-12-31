4d0f198cb211772e55e0365a1d71a19e3b5bf531
dlgudwn94 <dlgudwn82@naver.com>
2024-12-31 12:28:18 +0900

    [modify] 로그인 페이지 css 수정

--- src/components/Login/loginFormCss.js
+++ src/components/Login/loginFormCss.js
@@ -1,25 +1,27 @@
 import { css } from 'lit';
 
 export const loginFormCss = css`
+  .form-container {
+    height: 100vh;
+  }
+
   .login-section {
     max-width: 30rem;
     margin: 0 auto;
     padding: 2rem;
-    margin-top: 2rem;
   }
 
   h1 {
     font-size: 2rem;
     font-weight: 600;
-    margin-bottom: 2rem;
+    margin-bottom: 1rem;
     color: var(--white);
   }
 
   .help-links {
     display: flex;
     justify-content: center;
-    padding-right: 2rem;
-    padding-left: 2rem;
+    padding: 0.5rem;
     gap: 1rem;
   }
 
